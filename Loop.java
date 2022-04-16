02d0b5f02380c578a7478147d7e0b37cf96a8051
Nishar Ahamed H <91793825+Nishar-ahamed-h@users.noreply.github.com>
2022-04-16 18:55:26 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,21 @@
+package hackerRank;
+import java.util.*;
+import java.io.*;
+public class Loop {
+	    public static void main(String []arg){
+	        Scanner in = new Scanner(System.in);
+	        int t=in.nextInt();
+	        for(int i=0;i<t;i++){
+	           int a = in.nextInt();
+	           int b = in.nextInt();
+	           int n = in.nextInt();
+	           for(int j = 0; j < n; j++){
+	              a = a + (int)Math.pow(2,j)*b;
+	              System.out.print(a + " ");
+	             }
+	              System.out.println("");
+	             }
+	          in.close();
+	        }
+	    }
+
